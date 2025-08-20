c68927a47c590854206876576a61697399e60c74
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-08-20 18:49:07 +0200

    Declare *_by_geom and *_by_id tables

--- src/duckdb_quackosm/view.sql
+++ src/duckdb_quackosm/view.sql
@@ -1,17 +1,22 @@
 INSTALL 'spatial';
 LOAD 'spatial';
 
-CREATE OR REPLACE TEMP VIEW node AS
+CREATE OR REPLACE TEMP VIEW node_by_geom AS
 SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}' WHERE feature_id < 'o';
+CREATE OR REPLACE TEMP VIEW node_by_id AS SELECT * FROM node_by_geom;
 
-CREATE OR REPLACE TEMP VIEW way AS
+CREATE OR REPLACE TEMP VIEW way_by_geom AS
 SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}' WHERE feature_id > 'w';
+CREATE OR REPLACE TEMP VIEW way_by_id AS SELECT * FROM way_by_geom;
 
-CREATE OR REPLACE TEMP VIEW relation AS
+CREATE OR REPLACE TEMP VIEW relation_by_geom AS
 SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}' WHERE feature_id > 'o' AND feature_id < 's';
+CREATE OR REPLACE TEMP VIEW relation_by_id AS SELECT * FROM relation_by_geom;
 
-CREATE OR REPLACE TEMP VIEW nwr AS
+CREATE OR REPLACE TEMP VIEW nwr_by_geom AS
 SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}';
+CREATE OR REPLACE TEMP VIEW nwr_by_id AS SELECT * FROM nwr_by_geom;
 
-CREATE OR REPLACE TEMP VIEW area AS
+CREATE OR REPLACE TEMP VIEW area_by_geom AS
 SELECT split_part(feature_id, '/', 2)::bigint + CASE feature_id[1] WHEN 'r' THEN 3600000000 ELSE 0 END AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, CASE feature_id[1] WHEN 'w' THEN 'w' ELSE 'a' END AS osm_type FROM '#{parquet}' wHERE feature_id > 'm' AND list_contains(['POLYGON', 'MULTIPOLYGON'], ST_GeometryType(geometry));
+CREATE OR REPLACE TEMP VIEW area_by_id AS SELECT * FROM area_by_geom;
--- src/postgres_osmosis/view.sql
+++ src/postgres_osmosis/view.sql
@@ -1,22 +1,27 @@
-CREATE OR REPLACE TEMP VIEW node AS
+CREATE OR REPLACE TEMP VIEW node_by_geom AS
 SELECT id, version, tstamp, changeset_id, user_id, tags, NULL::bigint[] AS nodes, NULL::jsonb AS members, geom, 'n' AS osm_type FROM nodes;
+CREATE OR REPLACE TEMP VIEW node_by_id AS SELECT * FROM node;
 
-CREATE OR REPLACE TEMP VIEW way AS
+CREATE OR REPLACE TEMP VIEW way_by_geom AS
 SELECT id, version, tstamp, changeset_id, user_id, tags, nodes, NULL::jsonb AS members, linestring AS geom, 'w' AS osm_type FROM ways;
+CREATE OR REPLACE TEMP VIEW way_by_id AS SELECT * FROM way;
 
-CREATE OR REPLACE TEMP VIEW relation AS
+CREATE OR REPLACE TEMP VIEW relation_by_geom AS
 SELECT id, version, tstamp, changeset_id, user_id, tags,  NULL::bigint[] AS nodes, NULL::jsonb AS members, NULL::geometry AS geom, 'r' AS osm_type FROM relations;
+CREATE OR REPLACE TEMP VIEW relation_by_id AS SELECT * FROM relation;
 
-CREATE OR REPLACE TEMP VIEW nwr AS
-SELECT * FROM node
+CREATE OR REPLACE TEMP VIEW nwr_by_geom AS
+SELECT * FROM node_by_geom
 UNION ALL
-SELECT * FROM way
+SELECT * FROM way_by_geom
 UNION ALL
-SELECT * FROM relation
+SELECT * FROM relation_by_geom
 ;
+CREATE OR REPLACE TEMP VIEW nwr_by_id AS SELECT * FROM nwr_by_geom;
 
-CREATE OR REPLACE TEMP VIEW area AS
+CREATE OR REPLACE TEMP VIEW area_by_geom AS
 SELECT id, NULL::integer AS version, NULL::timestamp without time zone AS tstamp, NULL::bigint AS changeset_id, NULL::integer AS user_id, tags, NULL::bigint[] AS nodes, NULL::jsonb AS members, poly AS geom, 'a' AS osm_type FROM multipolygons WHERE id > 3600000000
 UNION ALL
 SELECT id, version, tstamp::timestamp with time zone AS created, changeset_id, user_id, tags, nodes AS nodes, NULL::jsonb AS members, ST_MakePolygon(linestring)::geometry(Geometry,4326) AS geom, 'w' AS osm_type FROM ways WHERE id < 3600000000 AND ST_IsClosed(linestring) AND ST_Dimension(ST_MakePolygon(linestring)) = 2
 ;
+CREATE OR REPLACE TEMP VIEW area_by_id AS SELECT * FROM area_by_geom;
