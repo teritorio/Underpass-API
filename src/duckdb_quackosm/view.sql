30abdd6f2c5ee1f9f6dc58458571a2d727b6de46
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-08-22 01:03:57 +0200

    Fix bbox

--- src/duckdb_quackosm/view.sql
+++ src/duckdb_quackosm/view.sql
@@ -27,11 +27,11 @@ CREATE OR REPLACE TEMP VIEW relation_by_id AS
 SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, 'r' AS osm_type FROM read_parquet('#{parquet}_relations_by_id');
 
 CREATE OR REPLACE TEMP VIEW nwr_by_geom AS
-SELECT id, version, created, changeset, uid, tags, nodes, members, geom, osm_type FROM node_by_geom
+SELECT id, version, created, changeset, uid, tags, nodes, members, geom, bbox, osm_type FROM node_by_geom
 UNION ALL
-SELECT id, version, created, changeset, uid, tags, nodes, members, geom, osm_type FROM way_by_geom
+SELECT id, version, created, changeset, uid, tags, nodes, members, geom, bbox, osm_type FROM way_by_geom
 UNION ALL
-SELECT id, version, created, changeset, uid, tags, nodes, members, geom, osm_type FROM relation_by_geom
+SELECT id, version, created, changeset, uid, tags, nodes, members, geom, bbox, osm_type FROM relation_by_geom
 ;
 
 CREATE OR REPLACE TEMP VIEW nwr_by_id AS
