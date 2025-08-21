c59106640df8377768a0bf7f0b206d19611709ba
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-08-21 13:32:44 +0200

    Use duckdb bbox

--- src/duckdb_quackosm/init.sql
+++ src/duckdb_quackosm/init.sql
@@ -1,6 +1,72 @@
 INSTALL 'spatial';
 LOAD 'spatial';
 
+COPY (
+SELECT
+    split_part(feature_id, '/', 2)::bigint AS id,
+    tags,
+    geometry AS geom,
+    STRUCT_PACK(
+        xmin := ST_XMin(geometry),
+        ymin := ST_YMin(geometry),
+        xmax := ST_XMax(geometry),
+        ymax := ST_YMax(geometry)
+    ) AS geom_bbox
+FROM
+    '#{parquet}'
+WHERE
+    feature_id[1] = 'n'
+ORDER BY
+    ST_Hilbert(
+        geometry,
+        ST_Extent(ST_MakeEnvelope(-180, -90, 180, 90))
+    )
+) TO '#{parquet}_nodes_by_geom' (FORMAT PARQUET);
+
+COPY (
+SELECT
+    split_part(feature_id, '/', 2)::bigint AS id,
+    tags,
+    geometry AS geom,
+    STRUCT_PACK(
+        xmin := ST_XMin(geometry),
+        ymin := ST_YMin(geometry),
+        xmax := ST_XMax(geometry),
+        ymax := ST_YMax(geometry)
+    ) AS geom_bbox
+FROM
+    '#{parquet}'
+WHERE
+    feature_id[1] = 'w'
+ORDER BY
+    ST_Hilbert(
+        geometry,
+        ST_Extent(ST_MakeEnvelope(-180, -90, 180, 90))
+    )
+) TO '#{parquet}_ways_by_geom' (FORMAT PARQUET);
+
+COPY (
+SELECT
+    split_part(feature_id, '/', 2)::bigint AS id,
+    tags,
+    geometry AS geom,
+    STRUCT_PACK(
+        xmin := ST_XMin(geometry),
+        ymin := ST_YMin(geometry),
+        xmax := ST_XMax(geometry),
+        ymax := ST_YMax(geometry)
+    ) AS geom_bbox
+FROM
+    '#{parquet}'
+WHERE
+    feature_id[1] = 'r'
+ORDER BY
+    ST_Hilbert(
+        geometry,
+        ST_Extent(ST_MakeEnvelope(-180, -90, 180, 90))
+    )
+) TO '#{parquet}_relations_by_geom' (FORMAT PARQUET);
+
 CREATE OR REPLACE TEMP VIEW node_by_geom AS
 SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}' WHERE feature_id < 'o';
 
@@ -17,5 +83,5 @@ CREATE OR REPLACE TEMP VIEW area_by_geom AS
 SELECT split_part(feature_id, '/', 2)::bigint + CASE feature_id[1] WHEN 'r' THEN 3600000000 ELSE 0 END AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, CASE feature_id[1] WHEN 'w' THEN 'w' ELSE 'a' END AS osm_type FROM '#{parquet}' wHERE feature_id > 'm' AND list_contains(['POLYGON', 'MULTIPOLYGON'], ST_GeometryType(geometry));
 
 COPY (SELECT id, tags, geom FROM node_by_geom ORDER BY id) TO '#{parquet}_nodes_by_id' (FORMAT PARQUET);
-COPY (SELECT id, nodes, tags, geom FROM way_by_geom) TO '#{parquet}_ways_by_id' (FORMAT PARQUET);
-COPY (SELECT id, NULL::bigint[] AS nodes, tags, geom FROM relation_by_geom) TO '#{parquet}_relations_by_id' (FORMAT PARQUET);
+COPY (SELECT id, tags, geom FROM way_by_geom) TO '#{parquet}_ways_by_id' (FORMAT PARQUET);
+COPY (SELECT id, tags, geom FROM relation_by_geom) TO '#{parquet}_relations_by_id' (FORMAT PARQUET);
--- src/duckdb_quackosm/view.sql
+++ src/duckdb_quackosm/view.sql
@@ -2,25 +2,30 @@ INSTALL 'spatial';
 LOAD 'spatial';
 
 CREATE OR REPLACE TEMP VIEW node_by_geom AS
-SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}' WHERE feature_id < 'o';
+SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, geom_bbox, 'n' AS osm_type FROM read_parquet('#{parquet}_nodes_by_geom');
 
 CREATE OR REPLACE TEMP VIEW node_by_id AS
-SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, 'n' AS osm_type FROM read_parquet('#{parquet}_nodes_by_id') WHERE osm_type = 'n';
+SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, 'n' AS osm_type FROM read_parquet('#{parquet}_nodes_by_id');
 
 CREATE OR REPLACE TEMP VIEW way_by_geom AS
-SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}' WHERE feature_id > 'w';
+SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, geom_bbox, 'w' AS osm_type FROM read_parquet('#{parquet}_ways_by_geom');
 
 CREATE OR REPLACE TEMP VIEW way_by_id AS
-SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, nodes, NULL::json AS members, geom, 'w' AS osm_type FROM read_parquet('#{parquet}_ways_by_id') WHERE osm_type = 'w';
+SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, 'w' AS osm_type FROM read_parquet('#{parquet}_ways_by_id') WHERE osm_type = 'w';
 
 CREATE OR REPLACE TEMP VIEW relation_by_geom AS
-SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}' WHERE feature_id > 'o' AND feature_id < 's';
+SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, geom_bbox, 'r' AS osm_type FROM read_parquet('#{parquet}_relations_by_geom');
 
 CREATE OR REPLACE TEMP VIEW relation_by_id AS
-SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, 'r' AS osm_type FROM read_parquet('#{parquet}_relations_by_id') WHERE osm_type = 'r';
+SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, 'r' AS osm_type FROM read_parquet('#{parquet}_relations_by_id');
 
 CREATE OR REPLACE TEMP VIEW nwr_by_geom AS
-SELECT split_part(feature_id, '/', 2)::bigint AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, feature_id[1] AS osm_type FROM '#{parquet}';
+SELECT * FROM node_by_geom
+UNION ALL
+SELECT * FROM way_by_geom
+UNION ALL
+SELECT * FROM relation_by_geom
+;
 
 CREATE OR REPLACE TEMP VIEW nwr_by_id AS
 SELECT * FROM node_by_id
@@ -31,10 +36,13 @@ SELECT * FROM relation_by_id
 ;
 
 CREATE OR REPLACE TEMP VIEW area_by_geom AS
-SELECT split_part(feature_id, '/', 2)::bigint + CASE feature_id[1] WHEN 'r' THEN 3600000000 ELSE 0 END AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geometry AS geom, CASE feature_id[1] WHEN 'w' THEN 'w' ELSE 'a' END AS osm_type FROM '#{parquet}' wHERE feature_id > 'm' AND list_contains(['POLYGON', 'MULTIPOLYGON'], ST_GeometryType(geometry));
+SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, geom_bbox, 'w' AS osm_type FROM read_parquet('#{parquet}_ways_by_geom') wHERE list_contains(['POLYGON', 'MULTIPOLYGON'], ST_GeometryType(geom))
+UNION ALL
+SELECT id + 3600000000 AS id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, geom_bbox, 'a' AS osm_type FROM read_parquet('#{parquet}_relations_by_geom') wHERE list_contains(['POLYGON', 'MULTIPOLYGON'], ST_GeometryType(geom))
+;
 
 CREATE OR REPLACE TEMP VIEW area_by_id AS
-SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, nodes, NULL::json AS members, geom, 'w' AS osm_type FROM read_parquet('#{parquet}_ways_by_id')
+SELECT id, NULL::int AS version, NULL::timestamp AS created, NULL::int AS changeset, NULL::int AS uid, tags, NULL::bigint[] AS nodes, NULL::json AS members, geom, 'w' AS osm_type FROM read_parquet('#{parquet}_ways_by_id')
 wHERE
     list_contains(['POLYGON', 'MULTIPOLYGON'], ST_GeometryType(geom))
 UNION ALL
